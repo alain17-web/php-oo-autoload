614a6ef50faa6101fa187ff6f494781603e03ebd
mikhawa <michaeljpitz@gmail.com>
2021-03-12 15:59:05 +0100

    getAllNewsInTheSection

--- model/TheNewsManager.php
+++ model/TheNewsManager.php
@@ -1,14 +1,13 @@
 <?php
+
 /*
  * On récupère de la classe abstraite les propriétés et méthodes, on doit utiliser les méthodes publiques obligatoires se trouvant dans l'interface
  */
 
-class TheNewsManager extends ManagerAbstract implements ManagerInterface
-{
+class TheNewsManager extends ManagerAbstract implements ManagerInterface {
 
     // méthode obligatoire venant de l'interface
-    public function getAll(): array
-    {
+    public function getAll(): array {
         $sql = "SELECT * FROM TheNews ORDER BY theNewsDate DESC";
         $recup = $this->db->query($sql);
         if (!$recup->rowCount()) {
@@ -23,8 +22,7 @@ class TheNewsManager extends ManagerAbstract implements ManagerInterface
     }
 
     // Affichage des news pour la page d'accueil
-    public function getAllHomePage(): array
-    {
+    public function getAllHomePage(): array {
         $sql = "SELECT n.theNewsTitle, n.theNewsSlug, LEFT(n.theNewsText,180) AS theNewsText, n.theNewsDate, n.theUserIdtheUser,
                     u.theUserLogin,
                     GROUP_CONCAT(s.theSectionName SEPARATOR '|||') as theSectionName, GROUP_CONCAT(s.idtheSection) as idtheSection
@@ -53,7 +51,6 @@ class TheNewsManager extends ManagerAbstract implements ManagerInterface
             $instanceNews->theUserLogin = $item['theUserLogin']; // theUserLogin
             $instanceNews->idtheSection = $item['idtheSection']; // idtheSection
             $instanceNews->theSectionName = $item['theSectionName']; // theSectionName
-
             // on coupe le texte récupéré grâce à son getter "$instanceNews->getTheNewsText()" en utilisant le méthode statique "TheNews::cuteTheText($instanceNews->getTheNewsText(),160)" donc à 160 caractères, puis je réattribue le tout avec "$instanceNews->setTheNewsText(...)"
             $instanceNews->setTheNewsText(TheNewsManager::cuteTheText($instanceNews->getTheNewsText(), 160));
             $news[] = $instanceNews;
@@ -62,8 +59,7 @@ class TheNewsManager extends ManagerAbstract implements ManagerInterface
     }
 
     // Détail d'un News chargée par un slug
-    public function getDetailNews(string $slug): array
-    {
+    public function getDetailNews(string $slug): array {
         $sql = "SELECT n.theNewsTitle, n.theNewsSlug, n.theNewsText, n.theNewsDate, n.theUserIdtheUser,
                     u.theUserLogin,
                     GROUP_CONCAT(s.theSectionName SEPARATOR '|||') as theSectionName, GROUP_CONCAT(s.idtheSection) as idtheSection
@@ -80,41 +76,91 @@ class TheNewsManager extends ManagerAbstract implements ManagerInterface
         // requête préparée
         $prepare = $this->db->prepare($sql);
         // essai
-        try{
+        try {
             // exécution de la requête
             $prepare->execute([$slug]);
 
             // si pas de résultats
-            if(!$prepare->rowCount()) return [0=>"Cet article n'existe plus"];
+            if (!$prepare->rowCount())
+                return [0 => "Cet article n'existe plus"];
 
             $oneNews = $prepare->fetch(PDO::FETCH_ASSOC);
             // instanciations des résultats en objets de type TheNews
+            // instanciation avec les valeurs SQL (seulement les setters existants dans TheNews!)
+            $instanceNews = new TheNews($oneNews);
 
-                // instanciation avec les valeurs SQL (seulement les setters existants dans TheNews!)
-                $instanceNews = new TheNews($oneNews);
+            // utilisation du __set (MappingTableAbstract.php) pour ajouter les champs manquants venant d'autres tables (! parfait pour de l'affichage, on utilise pas ces champs pour un insert update ou delete, car il contourne l'encapsulation et la protection des setters) - les éléments dans item viennent de la requête SQL
+            $instanceNews->theUserLogin = $oneNews['theUserLogin']; // theUserLogin
+            $instanceNews->idtheSection = $oneNews['idtheSection']; // idtheSection
+            $instanceNews->theSectionName = $oneNews['theSectionName']; // theSectionName
 
-                // utilisation du __set (MappingTableAbstract.php) pour ajouter les champs manquants venant d'autres tables (! parfait pour de l'affichage, on utilise pas ces champs pour un insert update ou delete, car il contourne l'encapsulation et la protection des setters) - les éléments dans item viennent de la requête SQL
-                $instanceNews->theUserLogin = $oneNews['theUserLogin']; // theUserLogin
-                $instanceNews->idtheSection = $oneNews['idtheSection']; // idtheSection
-                $instanceNews->theSectionName = $oneNews['theSectionName']; // theSectionName
+            return [1 => $instanceNews];
 
-            return [1=>$instanceNews];
-
-        // erreur
-        }catch(PDOException $e){
-            return [0=>$e->getMessage()];
+            // erreur
+        } catch (PDOException $e) {
+            return [0 => $e->getMessage()];
         }
-
     }
 
     // on récupère tous les articles qui sont dans une section
     public function getAllNewsInTheSection(int $idsection): array {
+        $sql = "SELECT n.theNewsTitle, n.theNewsSlug, LEFT(n.theNewsText,180) AS theNewsText, n.theNewsDate, n.theUserIdtheUser,
+                    u.theUserLogin,
+                    GROUP_CONCAT(s.theSectionName SEPARATOR '|||') as theSectionName, GROUP_CONCAT(s.idtheSection) as idtheSection
+                FROM TheNews n 
+                    INNER JOIN TheUser u
+                        ON u.idtheUser = n.theUserIdtheUser
+                    LEFT JOIN theNews_has_theSection h
+                        ON h.theNews_idtheNews = n.idtheNews
+                    LEFT JOIN thesection s 
+                        ON s.idtheSection = h.theSection_idtheSection
+                WHERE s.idtheSection = ?        
+                GROUP BY n.idtheNews
+                ORDER BY n.theNewsDate DESC
+                ";
 
+        // requête préparée
+        $recup = $this->db->prepare($sql);
+
+        // essai / erreur
+        try {
+
+            // exécution de la requête
+            $recup->execute([$idsection]);
+
+            // Pas encore de News
+            if (!$recup->rowCount()) {
+                // envoi d'un tableau vide: l'erreur se trouve dans la vue
+                return [];
+            }
+
+            $array = $recup->fetchAll(PDO::FETCH_ASSOC);
+            // instanciations des résultats en objets de type TheNews
+            foreach ($array as $item) {
+                // instanciation avec les valeurs SQL (seulement les setters existants dans TheNews!)
+                $instanceNews = new TheNews($item);
+
+                // utilisation du __set (MappingTableAbstract.php) pour ajouter les champs manquants venant d'autres tables (! parfait pour de l'affichage, on utilise pas ces champs pour un insert update ou delete, car il contourne l'encapsulation et la protection des setters) - les éléments dans item viennent de la requête SQL
+                $instanceNews->theUserLogin = $item['theUserLogin']; // theUserLogin
+                $instanceNews->idtheSection = $item['idtheSection']; // idtheSection
+                $instanceNews->theSectionName = $item['theSectionName']; // theSectionName
+                
+                // on coupe le texte récupéré grâce à son getter "$instanceNews->getTheNewsText()" en utilisant le méthode statique "TheNews::cuteTheText($instanceNews->getTheNewsText(),160)" donc à 160 caractères, puis je réattribue le tout avec "$instanceNews->setTheNewsText(...)"
+                $instanceNews->setTheNewsText(TheNewsManager::cuteTheText($instanceNews->getTheNewsText(), 160));
+                $news[] = $instanceNews;
+            }
+            return $news;
+
+            // erreur SQL    
+        } catch (PDOException $ex) {
+            // envoie de l'erreur 2 => 404
+            return [2 => $ex->getMessage()];
+        }
     }
 
     # method public static cuteTheText
-    public static function cuteTheText(string $text, int $nb)
-    {
+
+    public static function cuteTheText(string $text, int $nb) {
         // on coupe à la longueur maximale voulue
         $cuteText = substr($text, 0, $nb);
         // on trouve le dernier espace dans ce texte
@@ -122,12 +168,11 @@ class TheNewsManager extends ManagerAbstract implements ManagerInterface
         // on coupe la chaine avec ce dernier caractère
         $final = substr($cuteText, 0, $positionLastSpace);
         return $final;
-
     }
 
     # method public static slugify
-    public static function slugify($text)
-    {
+
+    public static function slugify($text) {
         // replace non letter or digits by -
         $text = preg_replace('~[^\pL\d]+~u', '-', $text);
 
@@ -152,6 +197,5 @@ class TheNewsManager extends ManagerAbstract implements ManagerInterface
 
         return $text;
     }
-
 
 }
